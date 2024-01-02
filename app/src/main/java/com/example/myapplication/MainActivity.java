2e9919ca1af9dcd855dc085e2d1cf1a8b2c7049e
Polina Kolesnikova <alraunepolina@yandex.ru>
2024-01-02 14:40:43 +0100

    life cycle of app

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,13 +4,36 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int score1 = 0;
-    int score2=0;
+    int score2 = 0;
+
+    TextView result1;
+    TextView result2;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy");
+    }
+
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
@@ -22,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", "onCreate");
         setContentView(R.layout.activity_main);
         if(savedInstanceState!=null){
             score1=savedInstanceState.getInt("score1");
@@ -30,28 +54,35 @@ public class MainActivity extends AppCompatActivity {
         Button btnTeam1 = findViewById(R.id.btn_team_1);
         TextView result1 = findViewById(R.id.res_team_1);
 
-        result1.setText(String.valueOf(score1));
+        updateScore(result1, score1);
 
         Button btnTeam2 = findViewById(R.id.btn_team_2);
         TextView result2 = findViewById(R.id.res_team_2);
 
-        result2.setText(String.valueOf(score2));
+        updateScore(result2, score2);
         btnTeam1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                result1.setText(String.valueOf(++score1));
+                score1++;
+                updateScore(result1, score1);
             }
         });
 
         btnTeam2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                result2.setText(String.valueOf(++score2));
+                score2++;
+                updateScore(result2, score2);
             }
         });
 
 
+
+
+    }
+
+    void updateScore(TextView textView, int score){
+
+        textView.setText(String.valueOf(score));
     }
 }
